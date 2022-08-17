b3a82bbc5419b81809418977f1d3e162abf04748
Yash Lad <yashlad727@gmail.com>
2022-08-17 18:39:15 +0530

    all operators in js

--- JS_Basics/first.js
+++ JS_Basics/first.js
@@ -1,1 +1,47 @@
 //document.getElementById("demo").innerHTML = "My first JS";
+var b = 10;
+var c = b;
+
+console.log("#############")
+// Arithmetic Operators:
+c += 2;
+console.log(c);
+c -= 2;
+console.log(c);
+c *= 2;
+console.log(c);
+c /= 2;
+console.log(c);
+
+console.log("#############")
+// Assignment Operator:
+var x = 45;
+var y = 56;
+
+console.log(x==y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x > y);
+console.log(x < y);
+
+console.log("#############")
+// Logical Operators:
+
+var a1 = "LOGICAL and";
+console.log(a1);
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+var b1 = "LOGICAL or";
+console.log(b1);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log("#############")
+// console.log(c);
