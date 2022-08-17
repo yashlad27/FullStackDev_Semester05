49406c389c498939a37091321b3c935b5cfebcba
Yash Lad <yashlad727@gmail.com>
2022-08-17 20:54:02 +0530

    date methods and new keyword

--- JS_Basics/dates.js
+++ JS_Basics/dates.js
@@ -2,5 +2,10 @@
 
 let myDate = new Date();
 console.log(myDate);
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+console.log(myDate.getMinutes());
+console.log(myDate.getHours());
 
 console.log("#############")
