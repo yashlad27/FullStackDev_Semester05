eadbeb2756fe2bf440cfa38abb7fdb8944c06833
Yash Lad <yashlad727@gmail.com>
2022-08-17 18:50:48 +0530

    conditionals

---
+++ JS_Basics/functions.js
@@ -0,0 +1,22 @@
+// Functions in JavaScript:
+
+// DRY = donot repeat yourself -> basics of Functions     
+
+function avg(a, b){
+    return (a+b)/2;
+}
+
+c1 = avg(4, 6);
+c2 = avg(14, 16);
+console.log(c1, c2);
+
+// Conditionals in JavaScript:
+
+var age = 34;
+if(age>18){
+    console.log("You are not a kid!");
+}else {
+    console.log("You are a kid!");
+}
+
+console.log("#############");
