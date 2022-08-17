669bca8dc262ef947d253b4ee3b2bf87bd797a7f
Yash Lad <yashlad727@gmail.com>
2022-08-17 19:03:12 +0530

    travelling through the array

--- JS_Basics/arrays.js
+++ JS_Basics/arrays.js
@@ -13,4 +13,17 @@ arr.forEach(function(element){
     console.log(element);
 })
 
+// preference to let instead of var:
+let j=0;
+const a = 0; // a++ won't work here
+
+while(j<arr.length){
+    console.log(arr[j]);
+    j++;
+}
+
+do {
+    console.log(arr[j]);
+    j++;    
+}while(j<arr.length);
 console.log("#############")
