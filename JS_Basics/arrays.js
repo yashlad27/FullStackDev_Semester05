2bf936cccc6c3e1f9f1bbaf3e931217dfc9a176b
Yash Lad <yashlad727@gmail.com>
2022-08-17 18:57:22 +0530

    arrays in js

--- JS_Basics/arrays.js
+++ JS_Basics/arrays.js
@@ -1,1 +1,16 @@
 // ARRAYS IN JAVASCRIPT:
+
+var arr = [1, 2, 4, 6, 8, 3, 5];
+console.log(arr);
+
+// Method-01 to iterate through array:
+for(var i = 0; i<arr.length; i++){
+    console.log(arr[i]);
+}
+
+// Method-02 to iterate through array:
+arr.forEach(function(element){
+    console.log(element);
+})
+
+console.log("#############")
