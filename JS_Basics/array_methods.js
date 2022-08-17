e606a19186127638e80456736d7e49388cbb6e36
Yash Lad <yashlad727@gmail.com>
2022-08-17 19:17:52 +0530

    array methods

---
+++ JS_Basics/array_methods.js
@@ -0,0 +1,18 @@
+// Methods in Array:
+
+let myArr = ["Fan", "Camera", 34, null, true];
+
+console.log(myArr.length);
+
+myArr.push("YASH");
+console.log(myArr)
+
+myArr.sort();
+console.log(myArr);
+
+myArr.toString();
+console.log(myArr);
+
+myArr.pop();
+console.log(myArr);
+
