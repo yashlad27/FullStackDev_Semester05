b89a5654f9b5e67ffaea411e7625e405705bf90a
Yash Lad <yashlad727@gmail.com>
2022-10-05 21:46:32 +0530

    JavaScript Udemy Web Dev

---
+++ JS_Basics/ZTM_JavaScript/exercise01.js
@@ -0,0 +1,82 @@
+//Evaluate the below:
+
+// 5 + "34"
+console.log(5+"34")
+
+// 5 - "4"
+console.log(5-"4")
+
+// 10 % 5 : modulo
+console.log(10%5)
+
+// 5 % 10
+console.log(5%10)
+
+// "Java" + "Script"
+console.log("Java"+"Script")
+
+// " " + " "
+console.log(" "+" ")
+
+// " "+"0"
+console.log(" "+"0")
+
+// true + false
+console.log(true + false)
+
+// true + true
+console.log(true + true)
+
+// false + true
+console.log(false + true)
+
+// false - true
+console.log(false - true)
+
+// 3 - 4
+console.log(3 - 4)
+
+// "Bob" - "bill"
+console.log("Bob" - "bill")
+
+// nan -> not A number
+
+console.log("---------------------------------------------------")
+console.log("                 COMPARISON                        ")
+console.log("---------------------------------------------------")
+
+// 5 >= 1
+console.log(5>=1)
+
+// 0 === 1
+console.log(0 === 1)
+
+// 4 <= 1
+console.log(4<=1)
+
+// 1 != 1
+console.log(1!=1)
+
+// "A" > "B"
+console.log("A">"B")
+
+// "B" < "C"
+console.log("B"<"C")
+
+// "a" > "A"
+console.log("a">"A")
+
+// "b" < "A"
+console.log("b"<"A")
+
+// true === false
+console.log(true === false)
+
+// true != true
+console.log(true != true)
+
+// true !== false
+console.log(true !== false)
+
+// Make the string: "Hi There! It's "sunny" out" by using the + sign:
+console.log('Hi '+'There! '+'It\'s '+'\"sunny\"'+' out')
