af4e0ca88edcbdbdc4beedd490d51d1467f15e84
Yash Lad <yashlad727@gmail.com>
2022-10-11 15:12:36 +0530

    variables in js

---
+++ JS_Basics/ZTM_JavaScript/variables.js
@@ -0,0 +1,36 @@
+
+var yash = "yash";
+undefined
+yash
+'yash'
+var FirstName = "Enter first name: ";
+undefined
+FirstName
+
+'Enter first name: '
+prompt()
+'vniknjqiwpv'
+prompt("What is your username?")
+'yash'
+var first = prompt("Enter first number: ");
+undefined
+var first = prompt("Enter first number: "); 
+var second = prompt("Enter second number: ");
+undefined
+first
+'10'
+second
+'12'
+Number(first)
+10
+Number(second)
+12
+
+var sum = first + second
+undefined
+sum
+'1012'
+
+var intSUM = Number(first) + Number(second)
+undefined
+
