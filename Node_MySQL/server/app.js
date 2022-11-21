ea4bda138739ab8cdb12384f69416267eb02fe5b
Yash Lad <yashlad727@gmail.com>
2022-11-21 14:39:26 +0530

    connected to mySQL db with nodeJS

--- Node_MySQL/server/app.js
+++ Node_MySQL/server/app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
+const dbService = require('./dbService');
 
 app.use(cors());
 app.use(express.json());
