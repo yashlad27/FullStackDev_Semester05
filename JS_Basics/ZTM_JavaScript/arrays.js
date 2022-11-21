d38d9195839e568a15e560c6b342f065afd13c64
Yash Lad <yashlad727@gmail.com>
2022-11-21 13:17:28 +0530

    Adding lab folders

---
+++ JS_Basics/ZTM_JavaScript/arrays.js
@@ -0,0 +1,15 @@
+var list = ["tiger", "cat", "bear", "bird"]
+
+console.log(list[1]);
+
+var functionlist = [function apple() {
+    console.log("apple");
+}]
+
+var array_in_array = [
+    ["tiger", "cat", "bear", "bird"]
+]
+
+console.log(list.shift());
+console.log(list);
+
