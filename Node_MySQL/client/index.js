ac746bc290f70fd657beb48217a64cee04ec9728
Yash Lad <yashlad727@gmail.com>
2022-11-21 13:56:55 +0530

    html and css complete

---
+++ Node_MySQL/client/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    loadHTMLtable([]);
+});
+
+function loadHTMLtable(data) {
+    const table = document.querySelector('table tbody');
+    let tableHtml = "";
+
+    if(data.length === 0){
+
+    }
+}
