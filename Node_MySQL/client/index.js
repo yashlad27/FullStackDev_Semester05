999744dbba74b9e43f744e82b1ce6a06fd18b496
Yash Lad <yashlad727@gmail.com>
2022-11-21 13:59:15 +0530

    getting data from user using JS

--- Node_MySQL/client/index.js
+++ Node_MySQL/client/index.js
@@ -7,6 +7,7 @@ function loadHTMLtable(data) {
     let tableHtml = "";
 
     if(data.length === 0){
-
+        table.innerHTML = "<tr><td class='no-data' colspan='5'> No Data </td></tr>";
+        
     }
 }
