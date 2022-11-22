43fac30224298238e2092f354a942a518b1bdd9b
Yash Lad <yashlad727@gmail.com>
2022-11-22 11:45:23 +0530

    viva commit

--- Node_MySQL/server/app.js
+++ Node_MySQL/server/app.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
-
 // create
 app.post('/insert', (request, response) => {
     const { name } = request.body;
