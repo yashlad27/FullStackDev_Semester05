ed94ed1f1ea865128d4c57ccbbaab8519ca15d8e
Yash Lad <yashlad727@gmail.com>
2022-11-28 21:21:48 +0530

    basics of js

---
+++ JS_Basics/Dave_Grey/js/main.js
@@ -0,0 +1,29 @@
+// console.log("Hello World!");
+
+// console.log(typeof"Yash");
+
+const myVar = "Mathematics";
+
+console.log(myVar.length);
+
+console.log(myVar.charAt(4));
+
+console.log(myVar.indexOf("e"));
+
+console.log(myVar.slice(5, 9));
+
+console.log(myVar.toUpperCase());
+
+console.log(myVar.toLowerCase());
+
+console.log(myVar.split(""));
+
+const myNum = 42;
+const myFloat = 42.00;
+const myString = "42";
+
+console.log(myNum);
+
+console.log(myNum === myFloat);
+
+console.log(number(myString) === myNum);
