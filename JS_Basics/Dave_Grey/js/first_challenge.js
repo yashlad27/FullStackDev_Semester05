c11dabfe0e8d4ed645862877af4a4008e3b21564
Yash Lad <yashlad727@gmail.com>
2022-11-28 21:57:12 +0530

    math fun and random func

---
+++ JS_Basics/Dave_Grey/js/first_challenge.js
@@ -0,0 +1,8 @@
+console.log(Math.floor(Math.random() * 10 + 1));
+
+const myName = "Yahsaknrfjfbdbvuhew";
+const lenName = myName.length;
+
+console.log(myName.charAt(
+    Math.floor(Math.random() * lenName)
+));
