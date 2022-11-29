ce1b1a48045dc7db6cc67bd478262cab3bffa5ba
Yash Lad <yashlad727@gmail.com>
2022-11-29 22:09:17 +0530

    introduction to tailwind css

---
+++ JS_Basics/Dave_Grey/switch/main.js
@@ -0,0 +1,17 @@
+// Conditionals: Switch Statement
+
+switch(Math.floor(Math.random() * 7 + 1)) {
+    case 1:
+        console.log(1);
+        break;
+
+    case 2:
+        console.log(2);
+        break;
+
+    case 3:
+        console.log(3);
+        break;
+    default:
+        console.log("No Match");
+}
